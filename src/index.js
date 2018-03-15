dbe597d50d714f79d7b74d3c116c11128e24f7b1
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-03-15 20:27:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let arr = s.split('');
+
+    let N = 1;
+    for (let i = 0; i < pairs.length; i++) {
+        N *= Math.pow(pairs[i][0], pairs[i][1]);
+    }
+    if (N > 100000000)
+        return 0;
+
+    function greatestCommonDivisor (a, b) {
+        if (!b) {
+            return a;
+        }
+        return greatestCommonDivisor(b, a % b);
+    };
+
+    let count = 0;
+    for (let k = 0; k <= N; k++) {
+        for (let j = 0; j < arr.length; j++) {
+
+            if (((greatestCommonDivisor(k + j, N) === 1) && (arr[j] === '1')) || ((greatestCommonDivisor(k + j, N) !== 1) && (arr[j] === '0'))) {
+
+                count++;
+
+            }
+        }
+    }
+
+    return count % 1000000007;
 }
